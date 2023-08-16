a9560f6aafe9fbbc2bb8ae7d19f0012fd6e6c116
Tarek Sallam <tarek@sallam.ca>
2023-08-16 16:26:13 -0400

    Removing threlte

--- src/lib/three/Scene.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import { T } from "@threlte/core"
-</script>
-
-<T.PerspectiveCamera makeDefault position={[0, 0, 10]} />
-<T.Mesh position = {[0, 0, 0]}>
-    <T.IcosahedronGeometry args={[1, 1, 1]}/>
-</T.Mesh>
