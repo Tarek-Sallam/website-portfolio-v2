80cc2780cdbcf2cd14cd9d28b313b238e216b12d
Tarek Sallam <tarek@sallam.ca>
2023-08-16 20:21:27 -0400

    removed threlte

--- src/lib/ThreeApp.svelte
+++ src/lib/ThreeApp.svelte
@@ -1,21 +1,10 @@
 <script>
-    import { Canvas } from '@threlte/core'
-    import Scene from './three/Scene.svelte'
+	import { onMount } from 'svelte';
+	let container;
+
+	onMount(() => {
+		// RUN THREE STUFF HERE
+	});
 </script>
 
-<div class="canvas">
-<Canvas useLegacyLights = {false}>
-    <Scene />
-</Canvas>
-</div>
-
-<style>
-    .canvas {
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-        z-index: 1;
-    }
-</style>
+<div bind:this={container} />
