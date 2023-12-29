776b8747c813225f91f57b24b68001d8702a9555
Tarek Sallam <tarek@sallam.ca>
2023-12-28 19:46:14 -0500

    amoount of name elements react to window resize

--- src/lib/AnimatedName.svelte
+++ src/lib/AnimatedName.svelte
@@ -3,14 +3,20 @@
 	import './css/AnimatedName.css';
 	import { gsap } from 'gsap';
 
+	//HTML element refs
 	let firstRef;
 	let lastRef;
 
+	let firstWrapperRef;
+	let lastWrapperRef;
+
+	// list of names that will be added and dynamically changed on resize
 	let extraFirstNames = [];
 	let extraLastNames = [];
 
+	// get the number of names algorithm
 	function getNumberOfNames(width) {
-		let count = 1;
+		let count = 0;
 		let remainder = window.innerWidth - width;
 		while (remainder > 0) {
 			count++;
@@ -19,6 +25,7 @@
 		return count;
 	}
 
+	// sets the name list with proper object given the amount of names
 	function setNamesList(n, n2) {
 		extraFirstNames = [];
 		extraLastNames = [];
@@ -44,26 +51,37 @@
 				}
 			}
 		}
+		return extraFirstNames, extraLastNames;
 	}
 
-	onMount(() => {
+	function animateNames(namesWrapper, width, direction) {}
+
+	// on window resize
+	function handleResize() {
 		const numFirstNames = getNumberOfNames(firstRef.offsetWidth);
+		console.log(firstRef.offsetWidth);
+		console.log(window.innerWidth);
 		const numLastNames = getNumberOfNames(lastRef.offsetWidth);
-		extraFirstNames = [];
-		extraLastNames = [];
-		setNamesList(numFirstNames, numLastNames);
+		extraFirstNames, (extraLastNames = setNamesList(numFirstNames, numLastNames));
+	}
+
+	// on mount set names using resize handle
+	onMount(() => {
+		handleResize();
 	});
 </script>
 
+<svelte:window on:resize={handleResize} />
+
 <div class="name-container">
-	<span class="name-wrapper">
+	<span class="name-wrapper" bind:this={firstWrapperRef}>
 		<p class="overlay first-name">TAREK</p>
 		<p bind:this={firstRef} class="outline first-name">TAREK</p>
 		{#each extraFirstNames as name}
 			<p class={name.classes}>TAREK</p>
 		{/each}
 	</span>
-	<span class="name-wrapper">
+	<span class="name-wrapper" bind:this={lastWrapperRef}>
 		<p class="overlay last-name">SALLAM</p>
 		<p bind:this={lastRef} class="outline last-name">SALLAM</p>
 		{#each extraLastNames as name}
